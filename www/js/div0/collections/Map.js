fad6f998b9c9f785fd6f85411d8bb2e94c9b8a9f
ilay <djyamato@mail.ru>
2016-09-01 14:34:48 +0400

    init commit

---
+++ www/js/div0/collections/Map.js
@@ -0,0 +1,50 @@
+/// <reference path="iterators/MapIterator.ts"/>
+var Map = (function () {
+    function Map(id) {
+        this.keys = new Array();
+        if (id) {
+            this.id = id;
+        }
+        this.items = {};
+    }
+    Map.prototype.removeKey = function (key) {
+        var index = this.keys.indexOf(key);
+        this.keys.splice(index, 1);
+    };
+    Map.prototype.add = function (key, value) {
+        this.items[key] = value;
+        this.keys.push(key);
+    };
+    Map.prototype.remove = function (key) {
+        delete this.items[key];
+        // remove key
+        this.removeKey(key);
+    };
+    Map.prototype.clear = function () {
+        this.keys = new Array();
+        this.items = {};
+    };
+    Map.prototype.has = function (key) {
+        return key in this.items;
+    };
+    Map.prototype.get = function (key) {
+        return this.items[key];
+    };
+    Map.prototype.getKeys = function () {
+        return this.keys;
+    };
+    Map.prototype.size = function () {
+        return this.keys.length;
+    };
+    Map.prototype.getIterator = function () {
+        return new MapIterator(this);
+    };
+    Map.prototype.setId = function (id) {
+        this.id = id;
+    };
+    Map.prototype.getId = function () {
+        return this.id;
+    };
+    return Map;
+})();
+//# sourceMappingURL=Map.js.map
---
+++ www/js/div0/collections/Map.ts
@@ -0,0 +1,60 @@
+/// <reference path="iterators/MapIterator.ts"/>
+class Map<T> {
+    private id:string;
+    private items: { [key: string]: T };
+    private keys:string[] = new Array();
+
+    constructor(id) {
+        if(id){
+            this.id = id;
+        }
+        this.items = {};
+    }
+
+    private removeKey(key):void{
+        var index = this.keys.indexOf(key);
+        this.keys.splice(index, 1);
+    }
+
+    add(key: string, value: T): void {
+        this.items[key] = value;
+        this.keys.push(key);
+    }
+
+    remove(key: string): void {
+        delete this.items[key];
+
+        // remove key
+        this.removeKey(key);
+    }
+
+    clear():void{
+        this.keys = new Array();
+        this.items = {};
+    }
+
+    has(key: string): boolean {
+        return key in this.items;
+    }
+
+    get(key: string): T {
+        return this.items[key];
+    }
+
+    getKeys():string[]{
+        return this.keys;
+    }
+
+    size(): number {
+        return this.keys.length;
+    }
+    getIterator():MapIterator{
+        return new MapIterator(this);
+    }
+    setId(id:string):void{
+        this.id = id;
+    }
+    getId():string{
+        return this.id;
+    }
+}
